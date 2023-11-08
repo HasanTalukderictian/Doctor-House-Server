21fde2cff1363f5417d46177c4b69c76a05ea7df
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-11-08 21:15:48 +0600

    Deleted Api create

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await booksCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.put('/bookings/:id', async(req, res) => {
+      const updatedBookings = req.body;
+      
+    })
+
 
 
     // Connect the client to the server	(optional starting in v4.7)
